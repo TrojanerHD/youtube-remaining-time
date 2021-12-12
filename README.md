0f964d118cdf8f709da5e318cff419e852066f61
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2021-12-12 23:33:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# YouTube Remaining Time
+On mobile, YouTube can toggle between the total time of the video and time remaining for the video. I brought this feature to the browser with the addition that if you watch a video in a different speed, it will show you the time remaining if you keep that speed.
+
+## Requirements
++ Firefox 48.0 or higher
+
+## Installation
+Head over to https://github.com/TrojanerHD/youtube-remaining-time/releases and click on the .xpi file of the newest release. There should be a popup asking whether you allow the addon to be installed. Click “Add”.
+
+## Gallery
+Normal speed:
+![Progress bar of YouTube video in normal speed](resources/normal-speed.jpg)
+1.5x speed:
+![YouTube video in 1.5x speed](resources/1.5-speed.jpg)
+
+## Development
+### Prerequesites
++ TypeScript
++ Git
++ Editor/IDE
+
+### Compiling
+Run `tsc -p .` in the root directory of this project
+
+## Limitations / Additional Notes
++ This plugin will not update automatically. If it does not work for you, check the [releases](https://github.com/TrojanerHD/youtube-remaining-time/releases) page to check if there is a new version
++ I do not guarantee to update this addon
++ Currently, this plugin only works for the domain `youtube.com`. Other countries's domains (like `youtube.de`) are not supported
++ The counter updates are not in the same millisecond as the updates of the YouTube time display (meaning the remaining time does not instantly change with the current time, but somewhere in the same second)
++ This plugin is only localized in English. If you run YouTube in a different language, all text from the plugin will still be English
+
+However, feel free to open [issues](https://github.com/TrojanerHD/youtube-remaining-time/issues/new) if the extension does not work for you (make sure you have the newest version installed before reporting an issue) or if you have a suggestion
---
+++ remaining-time.ts
@@ -0,0 +1,72 @@
+const settingsButton: HTMLButtonElement = document.querySelector(
+  'button.ytp-button:nth-child(9)'
+);
+settingsButton.click();
+settingsButton.click();
+let updateElement: HTMLSpanElement | undefined;
+function main(): void {
+  const speedElement: HTMLDivElement = document.querySelector(
+    'div.ytp-menuitem:nth-child(1) > div:nth-child(3)'
+  );
+  const speed: number = !isNaN(Number(speedElement.innerHTML))
+    ? Number(speedElement.innerHTML)
+    : 1;
+
+  let remainingTime: string = document.querySelector(
+    'span.ytp-time-duration:nth-child(3)'
+  ).innerHTML;
+  if (remainingTime.split(':').length === 2)
+    remainingTime = `0:${remainingTime}`;
+  let remainingDate: Date = new Date(`1970 ${remainingTime}`);
+  let currentTime: string =
+    document.querySelector('.ytp-time-current').innerHTML;
+  if (currentTime.split(':').length === 2) currentTime = `0:${currentTime}`;
+  remainingDate = toUTC(remainingDate);
+  remainingDate.setTime(
+    (remainingDate.getTime() -
+      toUTC(new Date(`1970 ${currentTime}`)).getTime()) /
+      speed
+  );
+  remainingDate = toLocal(remainingDate);
+  if (!updateElement) {
+    const mainSpan: HTMLSpanElement = document.createElement('span');
+    mainSpan.appendChild(
+      document.createTextNode(`(Remaining: ${parseTime(remainingDate)})`)
+    );
+
+    const seperatorSpan: HTMLSpanElement = document.createElement('span');
+    seperatorSpan.appendChild(document.createTextNode(' '));
+
+    const timeDisplay: HTMLSpanElement = document.querySelector(
+      '.ytp-time-display > span:nth-child(2)'
+    );
+
+    timeDisplay.appendChild(seperatorSpan);
+    updateElement = timeDisplay.appendChild(mainSpan);
+    return;
+  }
+  updateElement.innerHTML = `(Remaining: ${parseTime(remainingDate)})`;
+}
+
+main();
+setInterval(main, 1000);
+
+function parseTime(time: Date): string {
+  let result: string = '';
+  if (time.getHours() !== 0) result += `${time.getHours()}:`;
+  if (time.getMinutes() < 10) result += '0';
+  result += `${time.getMinutes()}:`;
+  if (time.getSeconds() < 10) result += '0';
+  result += time.getSeconds();
+  return result;
+}
+
+function toUTC(time: Date): Date {
+  time.setMinutes(time.getMinutes() - time.getTimezoneOffset());
+  return time;
+}
+
+function toLocal(time: Date): Date {
+  time.setMinutes(time.getMinutes() + time.getTimezoneOffset());
+  return time;
+}
